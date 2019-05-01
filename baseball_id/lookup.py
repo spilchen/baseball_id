0bb2a5a63a79f3c06d546439351dd0b81e0479d9
Matt Spilchen <spilchen@ca.ibm.com>
2019-04-30 21:52:22 -0400

    Add APIs for other data sources

--- baseball_id/lookup.py
+++ baseball_id/lookup.py
@@ -4,6 +4,19 @@ import pandas as pd
 
 
 class Lookup:
+    """Class that manages lookup of baseball players by various IDs.
+
+    All of the from_* APIs return a DataFrame that has baseball player
+    particulars for the IDs in the lookup.  For each player that it finds
+    it will a row in a panda's DataFrame that has:
+    - birth year
+    - bat handed, throw handed
+    - from various databases (e.g. MLB, baseball-reference.com, Yahoo!, ESPN,
+      CBS, etc.)
+      - name
+      - position in various databases
+      - ID
+    """
     def __init__(self):
         self.source = 'http://crunchtimebaseball.com/master.csv'
         self.df = None
@@ -16,16 +29,86 @@ class Lookup:
         if self.df is None:
             self.df = pd.read_csv(self.source, encoding='iso-8859-1')
 
-    def mlb_id(self, id):
-        """Lookup the given mlb_ID
+    def from_mlb_ids(self, ids):
+        """Lookup given a list of mlb_ID's
 
-        :param id: mlb_ID to lookup
-        :type id: int
-        :return: Particulars of the given mlb_ID lookup
-        :rtype: Series
+        Accepts a list of MLB IDs.  It will return a DataFrame containing
+        Series for players that match the MLB IDs passed in.  If nothing was
+        found an empty DataFrame is returned.
+
+        :param ids: mlb_ID's to lookup
+        :type ids: int list
+        :return: Players that match the given IDs.  If none are found an empty
+        DataFrame is returned.
+        :rtype: DataFrame
         """
         self._read_source()
-        lookup_df = self.df[self.df.mlb_id == id]
-        if lookup_df.size == 0:
-            raise KeyError("Could not player for mlb_ID {}".format(id))
-        return lookup_df.iloc[0]
+        return self.df[self.df.mlb_id.isin(ids)]
+
+    def from_yahoo_ids(self, ids):
+        """Lookup given a list of Yahoo! IDs
+
+        Accepts a list of IDs from Yahoo! fantasy baseball.  It will return a
+        DataFrame containing Series for players that match the Yahoo! IDs.  If
+        nothing was found an empty DataFrame is returned.
+
+        :param ids: Yahoo! IDs to lookup
+        :type ids: int list
+        :return: Players that match the given IDs.  If none are found an empty
+        DataFrame is returned.
+        :rtype: DataFrame
+        """
+        self._read_source()
+        return self.df[self.df.yahoo_id.isin(ids)]
+
+    def from_cbs_ids(self, ids):
+        """Lookup given a list of CBS IDs
+
+        Accepts a list of IDs from CBS fantasy baseball.  It will return a
+        DataFrame containing Series for players that match the IDs.  If nothing
+        was found an empty DataFrame is returned.
+
+        :param ids: CBS IDs to lookup
+        :type ids: int list
+        :return: Players that match the given IDs.  If none are found an empty
+        DataFrame is returned.
+        :rtype: DataFrame
+        """
+        self._read_source()
+        return self.df[self.df.cbs_id.isin(ids)]
+
+    def from_espn_ids(self, ids):
+        """Lookup given a list of ESPN IDs
+
+        Accepts a list of IDs from ESPN fantasy baseball.  It will return a
+        DataFrame containing Series for players that match the IDs.  If nothing
+        was found an empty DataFrame is returned.
+
+        :param ids: ESPN IDs to lookup
+        :type ids: int list
+        :return: Players that match the given IDs.  If no Series are found an
+        empty DataFrame is returned.
+        :rtype: DataFrame
+        """
+        self._read_source()
+        return self.df[self.df.espn_id.isin(ids)]
+
+    def from_fangraphs_ids(self, ids):
+        """Lookup given a list of FanGraph IDs
+
+        Accepts a list of IDs as maintained at fangraphs.com.  It will return a
+        DataFrame containing Series for players that match the IDs.  If nothing
+        was found an empty DataFrame is returned.
+
+        :param ids: FanGraph IDs to lookup
+        :type ids: int list
+        :return: Players that match the given IDs.  If no Series are found an
+        empty DataFrame is returned.
+        :rtype: DataFrame
+        """
+        self._read_source()
+        # The DataFrame for the fangraph ID column is type str.  This is
+        # because FanGraph ids can have character values.  Convert all of
+        # the input IDs to strings so that it finds the all numeric ones.
+        ids_as_str = [str(i) for i in ids]
+        return self.df[self.df.fg_id.isin(ids_as_str)]
--- baseball_id/tests/test_lookup.py
+++ baseball_id/tests/test_lookup.py
@@ -1,13 +1,63 @@
 #!/bin/python
 
-import pytest
 
-
-def test_lookup_AaronHill(lookup_obj):
-    s = lookup_obj.mlb_id(431094)
-    assert(s.mlb_name == 'Aaron Hill')
+def test_lookup_AaronHill_by_mlb_ID(lookup_obj):
+    s = lookup_obj.from_mlb_ids([431094])
+    print(s)
+    assert(len(s) == 1)
+    assert(s.iloc[0].mlb_name == 'Aaron Hill')
 
 
 def test_lookup_bad_ID(lookup_obj):
-    with pytest.raises(Exception):
-        lookup_obj.mlb_id(0)
+    s = lookup_obj.from_mlb_ids([0])
+    assert(len(s) == 0)
+
+
+def test_lookup_by_mlb_ids(lookup_obj):
+    s = lookup_obj.from_mlb_ids([643327, 554430, 621107, 435043])
+    print(s)
+    assert(len(s) == 4)
+    assert(s.iloc[0].mlb_name == 'Zach Duke')
+    assert(s.iloc[1].mlb_name == 'Zach Eflin')
+    assert(s.iloc[2].mlb_name == 'Zack Godley')
+    assert(s.iloc[3].mlb_name == 'Zack Wheeler')
+
+
+def test_lookup_by_yahoo_ids(lookup_obj):
+    s = lookup_obj.from_yahoo_ids([9320, 9449, 9317])
+    print(s)
+    assert(len(s) == 3)
+    assert(s.iloc[0].yahoo_name == 'Chaz Roe')
+    assert(s.iloc[1].yahoo_name == 'Christian Yelich')
+    assert(s.iloc[2].yahoo_name == 'Hyun-Jin Ryu')
+
+
+def test_lookup_by_cbs_ids(lookup_obj):
+    s = lookup_obj.from_cbs_ids([223481])
+    print(s)
+    assert(len(s) == 1)
+    assert(s.iloc[0].mlb_name == 'Kyle Lohse')
+    assert(s.iloc[0].cbs_name == 'Kyle Lohse')
+    assert(s.iloc[0].cbs_id == 223481)
+    assert(s.iloc[0].mlb_id == 346798)
+
+
+def test_lookup_by_espn_ids(lookup_obj):
+    s = lookup_obj.from_espn_ids([33080])
+    print(s)
+    assert(len(s) == 1)
+    assert(s.iloc[0].mlb_name == 'Adonis Garcia')
+    assert(s.iloc[0].espn_id == 33080)
+    assert(s.iloc[0].mlb_id == 611177)
+
+
+def test_lookup_by_fangraphs_ids(lookup_obj):
+    s = lookup_obj.from_fangraphs_ids([13677, 'sa739620'])
+    print(s)
+    assert(len(s) == 2)
+    assert(s.iloc[0].mlb_name == 'Corey Oswalt')
+    assert(s.iloc[0].fg_id == '13677')
+    assert(s.iloc[0].mlb_id == 621261)
+    assert(s.iloc[1].mlb_name == 'Thairo Estrada')
+    assert(s.iloc[1].fg_id == 'sa739620')
+    assert(s.iloc[1].mlb_id == 642731)
