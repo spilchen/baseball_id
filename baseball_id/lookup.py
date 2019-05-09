e523b99481fec5dde1c59b4fab93b136d88a38c3
Matt Spilchen <spilchen@ca.ibm.com>
2019-05-08 21:14:15 -0400

    Simplify API by removing requirement of Lookup object
    We had this Lookup object that was in a lookup.py file.  So a sample use
    of the API was:
    import baseball_id.lookup
    l = baseball_id.lookup.Lookup()
    l.from_mlb_ids([642731])
    
    With this change it was simplified to:
    import baseball_id.lookup
    baseball_id.lookup.from_mlb_ids([642731])

--- baseball_id/lookup.py
+++ baseball_id/lookup.py
@@ -3,8 +3,8 @@
 import pandas as pd
 
 
-class Lookup:
-    """Class that manages lookup of baseball players by various IDs.
+class Cache:
+    """Class that caches results of baseball ID lookups.
 
     All of the from_* APIs return a DataFrame that has baseball player
     particulars for the IDs in the lookup.  For each player that it finds
@@ -30,85 +30,110 @@ class Lookup:
             self.df = pd.read_csv(self.source, encoding='iso-8859-1')
 
     def from_mlb_ids(self, ids):
-        """Lookup given a list of mlb_ID's
-
-        Accepts a list of MLB IDs.  It will return a DataFrame containing
-        Series for players that match the MLB IDs passed in.  If nothing was
-        found an empty DataFrame is returned.
-
-        :param ids: mlb_ID's to lookup
-        :type ids: int list
-        :return: Players that match the given IDs.  If none are found an empty
-        DataFrame is returned.
-        :rtype: DataFrame
-        """
         self._read_source()
         return self.df[self.df.mlb_id.isin(ids)]
 
     def from_yahoo_ids(self, ids):
-        """Lookup given a list of Yahoo! IDs
-
-        Accepts a list of IDs from Yahoo! fantasy baseball.  It will return a
-        DataFrame containing Series for players that match the Yahoo! IDs.  If
-        nothing was found an empty DataFrame is returned.
-
-        :param ids: Yahoo! IDs to lookup
-        :type ids: int list
-        :return: Players that match the given IDs.  If none are found an empty
-        DataFrame is returned.
-        :rtype: DataFrame
-        """
         self._read_source()
         return self.df[self.df.yahoo_id.isin(ids)]
 
     def from_cbs_ids(self, ids):
-        """Lookup given a list of CBS IDs
-
-        Accepts a list of IDs from CBS fantasy baseball.  It will return a
-        DataFrame containing Series for players that match the IDs.  If nothing
-        was found an empty DataFrame is returned.
-
-        :param ids: CBS IDs to lookup
-        :type ids: int list
-        :return: Players that match the given IDs.  If none are found an empty
-        DataFrame is returned.
-        :rtype: DataFrame
-        """
         self._read_source()
         return self.df[self.df.cbs_id.isin(ids)]
 
     def from_espn_ids(self, ids):
-        """Lookup given a list of ESPN IDs
-
-        Accepts a list of IDs from ESPN fantasy baseball.  It will return a
-        DataFrame containing Series for players that match the IDs.  If nothing
-        was found an empty DataFrame is returned.
-
-        :param ids: ESPN IDs to lookup
-        :type ids: int list
-        :return: Players that match the given IDs.  If no Series are found an
-        empty DataFrame is returned.
-        :rtype: DataFrame
-        """
         self._read_source()
         return self.df[self.df.espn_id.isin(ids)]
 
     def from_fangraphs_ids(self, ids):
-        """Lookup given a list of FanGraph IDs
-
-        Accepts a list of IDs as maintained at fangraphs.com.  It will return a
-        DataFrame containing Series for players that match the IDs.  If nothing
-        was found an empty DataFrame is returned.
-
-        :param ids: FanGraph IDs to lookup
-        :type ids: int list
-        :return: Players that match the given IDs.  If no Series are found an
-        empty DataFrame is returned.
-        :rtype: DataFrame
-        """
         self._read_source()
         # The DataFrame for the fangraph ID column is type str.  This is
         # because FanGraph ids can have character values.  Convert all of
         # the input IDs to strings so that it finds the all numeric ones.
         ids_as_str = [str(i) for i in ids]
         return self.df[self.df.fg_id.isin(ids_as_str)]
+
+
+def from_mlb_ids(ids):
+    """Lookup given a list of mlb_ID's
+
+    Accepts a list of MLB IDs.  It will return a DataFrame containing
+    Series for players that match the MLB IDs passed in.  If nothing was
+    found an empty DataFrame is returned.
+
+    :param ids: mlb_ID's to lookup
+    :type ids: int list
+    :return: Players that match the given IDs.  If none are found an empty
+    DataFrame is returned.
+    :rtype: DataFrame
+    """
+    c = Cache()
+    return c.from_mlb_ids(ids)
+
+
+def from_yahoo_ids(self, ids):
+    """Lookup given a list of Yahoo! IDs
+
+    Accepts a list of IDs from Yahoo! fantasy baseball.  It will return a
+    DataFrame containing Series for players that match the Yahoo! IDs.  If
+    nothing was found an empty DataFrame is returned.
+
+    :param ids: Yahoo! IDs to lookup
+    :type ids: int list
+    :return: Players that match the given IDs.  If none are found an empty
+             DataFrame is returned.
+    :rtype: DataFrame
+    """
+    c = Cache()
+    return c.from_yahoo_ids(ids)
+
+
+def from_cbs_ids(self, ids):
+    """Lookup given a list of CBS IDs
+
+    Accepts a list of IDs from CBS fantasy baseball.  It will return a
+    DataFrame containing Series for players that match the IDs.  If nothing
+    was found an empty DataFrame is returned.
+
+    :param ids: CBS IDs to lookup
+    :type ids: int list
+    :return: Players that match the given IDs.  If none are found an empty
+             DataFrame is returned.
+    :rtype: DataFrame
+    """
+    c = Cache()
+    return c.from_cbs_ids(ids)
+
+
+def from_espn_ids(self, ids):
+    """Lookup given a list of ESPN IDs
+
+    Accepts a list of IDs from ESPN fantasy baseball.  It will return a
+    DataFrame containing Series for players that match the IDs.  If nothing
+    was found an empty DataFrame is returned.
+
+    :param ids: ESPN IDs to lookup
+    :type ids: int list
+    :return: Players that match the given IDs.  If no Series are found an
+             empty DataFrame is returned.
+    :rtype: DataFrame
+    """
+    c = Cache()
+    return c.from_espn_ids(ids)
+
+
+def from_fangraphs_ids(self, ids):
+    """Lookup given a list of FanGraph IDs
+
+    Accepts a list of IDs as maintained at fangraphs.com.  It will return a
+    DataFrame containing Series for players that match the IDs.  If nothing
+    was found an empty DataFrame is returned.
+
+    :param ids: FanGraph IDs to lookup
+    :type ids: int list
+    :return: Players that match the given IDs.  If no Series are found an
+             empty DataFrame is returned.
+    :rtype: DataFrame
+    """
+    c = Cache()
+    return c.from_fangraphs_ids(ids)
--- baseball_id/tests/conftest.py
+++ baseball_id/tests/conftest.py
@@ -7,7 +7,7 @@ import os
 
 @pytest.fixture()
 def lookup_obj():
-    lk = lookup.Lookup()
+    lk = lookup.Cache()
     dir_path = os.path.dirname(os.path.realpath(__file__))
     lk.set_source("{}/sample.master.csv".format(dir_path))
     yield lk
